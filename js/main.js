3f21d48d379e2620348be5d9220482a4e6d10bdf
Nicola <nginvernizzi1@gmail.com>
2020-10-29 14:30:34 +0100

    js done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log('test');
+var numbers = 5;
+
+var randomNumbers = [];
+
+for (var i = 0; i < numbers; i++) {
+    var randomNumber = getRandomNumbers();
+
+    randomNumbers.push(randomNumber);
+}
+
+alert(randomNumbers);
+
+var userNumbers = [];
+
+setTimeout(function() {
+    for (var i = 0; i < numbers; i++) {
+        var userNumber = parseInt(prompt('Inserisci'));
+        
+        if (randomNumbers.includes(userNumber)) {
+            userNumbers.push(userNumber);
+        }
+
+    }
+    
+    console.log(userNumbers);
+    console.log(userNumbers.length);
+
+}, 3000);
+
+
+
+function getRandomNumbers() {
+    return Math.floor(Math.random() * 100) + 1;
+}
